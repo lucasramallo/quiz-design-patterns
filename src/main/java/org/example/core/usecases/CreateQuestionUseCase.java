aa5a99e9a7ec146eb1a94faa8135246d28c5ec44
sabrina.costa <sabrina.costa.ext@synchro.com.br>
2024-03-24 13:32:50 -0300

    create: usecase createquestion

--- src/main/java/org/example/core/usecases/CreateQuestionUseCase.java
+++ src/main/java/org/example/core/usecases/CreateQuestionUseCase.java
@@ -1,5 +1,29 @@
 package org.example.core.usecases;
 
+import org.example.core.domain.Trail;
+import org.example.core.domain.Alternative;
+import org.example.core.interfaces.IQuestion;
+import org.example.core.repository.QuestionRepository;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class CreateQuestionUseCase {
-    
+    private Trail trail;
+    private QuestionRepository questionRepository;
+
+    public CreateQuestionUseCase(Trail trail, QuestionRepository questionRepository) {
+        this.trail = trail;
+        this.questionRepository = questionRepository;
+    }
+
+    public void createQuestion(String description, ArrayList<Alternative> alternatives) {
+        // Criar uma nova questão
+        IQuestion question = new Question(description, alternatives, trail);
+
+        // Adicionar a questão ao repositório
+        questionRepository.addQuestion(question);
+
+        System.out.println("Questão criada com sucesso!");
+    }
 }
